3619fa579636eac2b4b474166876176670f681ab
Shlok Jadeja <200130107019@gecg28.ac.in>
2023-03-01 16:16:44 +0530

    Final code Without Model Integration

--- lib/Screen/CameraScreen.dart
+++ lib/Screen/CameraScreen.dart
@@ -1,16 +1,76 @@
+// import 'package:flutter/material.dart';
+//
+//
+// class CameraScreen extends StatefulWidget {
+//   const CameraScreen({super.key});
+//
+//   @override
+//   State<CameraScreen> createState() => _CameraScreenState();
+// }
+//
+// class _CameraScreenState extends State<CameraScreen> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Text("Camera Screen");
+//   }
+// }
+
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 
 class CameraScreen extends StatefulWidget {
   const CameraScreen({super.key});
 
   @override
-  State<CameraScreen> createState() => _CameraScreenState();
+  // ignore: library_private_types_in_public_api
+  _MyAppState createState() => _MyAppState();
 }
 
-class _CameraScreenState extends State<CameraScreen> {
+class _MyAppState extends State<CameraScreen> {
   @override
   Widget build(BuildContext context) {
-    return Text("Camera Screen");
+    return const MaterialApp(home: InAppWebViewPage());
+  }
+}
+
+class InAppWebViewPage extends StatefulWidget {
+  const InAppWebViewPage({super.key});
+
+  @override
+  _InAppWebViewPageState createState() => new _InAppWebViewPageState();
+}
+
+class _InAppWebViewPageState extends State<InAppWebViewPage> {
+  late InAppWebViewController _webViewController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text("InAppWebView")),
+        body: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: InAppWebView(
+              initialUrlRequest: URLRequest(
+                  url: Uri.parse(
+                      "https://shlok-177.github.io/Converter/templates/Training.html")),
+              initialOptions: InAppWebViewGroupOptions(
+                crossPlatform: InAppWebViewOptions(
+                  mediaPlaybackRequiresUserGesture: false,
+                ),
+              ),
+              onWebViewCreated: (InAppWebViewController controller) {
+                _webViewController = controller;
+              },
+              androidOnPermissionRequest: (InAppWebViewController controller,
+                  String origin, List<String> resources) async {
+                return PermissionRequestResponse(
+                    resources: resources,
+                    action: PermissionRequestResponseAction.GRANT);
+              }),
+        ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,18 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:ssip_signverse_project/Screen/HomeScreen.dart';
-import 'package:ssip_signverse_project/Services/AuthClass.dart';
 import 'package:ssip_signverse_project/introduction_animation/introduction_animation_screen.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
-
-
-
 final storage = new FlutterSecureStorage();
-
 
 Future<void> main()async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await Permission.camera.request();
+  await Permission.microphone.request();
 
   SharedPreferences preferences = await SharedPreferences.getInstance();
   var email = preferences.getString('email');
@@ -24,6 +22,71 @@ Future<void> main()async{
     home: email == null ? IntroductionAnimationScreen() : HomeScreen(),
   ),);
 }
-
-
-
+//
+// import 'dart:async';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+// import 'package:permission_handler/permission_handler.dart';
+//
+// Future main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//
+//   await Permission.camera.request();
+//   await Permission.microphone.request();
+//
+//   runApp(const MyApp());
+// }
+//
+// class MyApp extends StatefulWidget {
+//   const MyApp({super.key});
+//
+//   @override
+//   // ignore: library_private_types_in_public_api
+//   _MyAppState createState() => _MyAppState();
+// }
+//
+// class _MyAppState extends State<MyApp> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(home: InAppWebViewPage());
+//   }
+// }
+//
+// class InAppWebViewPage extends StatefulWidget {
+//   const InAppWebViewPage({super.key});
+//
+//   @override
+//   _InAppWebViewPageState createState() => new _InAppWebViewPageState();
+// }
+//
+// class _InAppWebViewPageState extends State<InAppWebViewPage> {
+//   late InAppWebViewController _webViewController;
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         appBar: AppBar(title: const Text("InAppWebView")),
+//         body: Container(
+//           height: MediaQuery.of(context).size.height,
+//           width: MediaQuery.of(context).size.width,
+//           child: InAppWebView(
+//               initialUrlRequest: URLRequest(
+//                   url: Uri.parse(
+//                       "https://shlok-177.github.io/Converter/templates/Training.html")),
+//               initialOptions: InAppWebViewGroupOptions(
+//                 crossPlatform: InAppWebViewOptions(
+//                   mediaPlaybackRequiresUserGesture: false,
+//                 ),
+//               ),
+//               onWebViewCreated: (InAppWebViewController controller) {
+//                 _webViewController = controller;
+//               },
+//               androidOnPermissionRequest: (InAppWebViewController controller,
+//                   String origin, List<String> resources) async {
+//                 return PermissionRequestResponse(
+//                     resources: resources,
+//                     action: PermissionRequestResponseAction.GRANT);
+//               }),
+//         ));
+//   }
+// }
